87b4e380345bdd09f1c9f5fec856ffd0118e788a
Akshay <akshaypanchal67@gmail.com>
2019-07-28 12:49:29 -0400

    Create Home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import HomePage from './Components/HomePage';
 
 
 function App() {
   return (
     <div>
-          <h1>Hello World!</h1>
+          <HomePage />
     </div>
   );
 }
