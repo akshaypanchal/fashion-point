bdacb5a8fd660d5b1d64b8b9ae1d6f1d9fa1f1f2
Akshay <akshaypanchal67@gmail.com>
2019-08-21 23:29:27 -0400

    add header component to main website with styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import HomePage from './Pages/homepage/HomePage.component';
 import ShopPage from './Pages/shop/shop.component';
+import Header from './Components/header/header.component';
 import './App.css';
 
 
 function App() {
   return (
     <div>
+    	<Header />
+		{/* Routing for the Home and Shop page */}
     	<Switch>
         	  <Route exact path='/' component={HomePage} />
         	  <Route path='/shop' component={ShopPage} />
