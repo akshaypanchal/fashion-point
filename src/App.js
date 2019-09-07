b140e8f75f78e1eff5f45037f2f153767377c7e0
Akshay <akshaypanchal67@gmail.com>
2019-09-07 16:44:52 -0400

    add sign in component and sign in page with styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import HomePage from './Pages/homepage/HomePage.component';
 import ShopPage from './Pages/shop/shop.component';
 import Header from './Components/header/header.component';
+import SignInAndSignUpPage from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import './App.css';
 
 
@@ -14,6 +15,7 @@ function App() {
     	<Switch>
         	  <Route exact path='/' component={HomePage} />
         	  <Route path='/shop' component={ShopPage} />
+            <Route path='/signin' component={SignInAndSignUpPage} />
     	</Switch>
     </div>
   );
